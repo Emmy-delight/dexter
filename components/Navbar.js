f4201d5032d4156cccbd9d74bb3a0332f931ed9d
Emmy-delight <165961039+Emmy-delight@users.noreply.github.com>
2025-11-28 16:02:08 +0100

    added a form

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,5 @@
   "use client"
+import Link from "next/link";
 import { useState } from "react";
 import { IoIosMenu } from "react-icons/io";
 
@@ -8,17 +9,19 @@ export function Navbar () {
         setDropDown(!dropDown);
     }
     return (
-        <main className="bg-gray-100 shadow shadow-gray-300 w-full sticky top-0">
+        <main className="bg-gray-100 shadow shadow-gray-300 w-full sticky top-0 z-10">
             <div className="h-[50px] flex justify-between items-center px-5 ">
                  <h1 className="text-2xl font-bold  text-blue-500">Dexter</h1>
                  <ul className="hidden md:text-sm font-semibold md:flex gap-10 cursor-pointer">
-                    <li>Home</li>
+                    <Link href="/"><li>Home</li></Link>
                     <li>Contact-us</li>
                     <li>About-us</li>
                  </ul>
+                 <Link href="/auth/login">
                  <div className="hidden md:block">
                    <button className="w-15 h-[30px] bg-blue-500 rounded-md text-white">Login</button>
                  </div>
+                 </Link>
                  <div className="block md:hidden">
                     <IoIosMenu onClick={toggleMenu} className="text-3xl text-blue-500"/>
                  </div>      
