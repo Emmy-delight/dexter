6919f5cbf9e028856de53ef873c11a67e18711a8
Emmy-delight <165961039+Emmy-delight@users.noreply.github.com>
2025-12-15 11:47:14 +0100

    added delete function

--- app/dashboard/guest-list/[id]/page.js
+++ app/dashboard/guest-list/[id]/page.js
@@ -1,6 +1,48 @@
+      "use client"
+import { db } from "@/config/firebase.config";
+import { CircularProgress } from "@mui/material";
+import { deleteDoc, doc, getDoc } from "firebase/firestore";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { RiDeleteBin5Line } from "react-icons/ri";
 
 export default function GuestPage () {
+    const {id} = useParams();
+    const [guest,setGuest] = useState(null);
+    const [loading,setLoading] = useState();
+    const router = useRouter(); 
+    //Fetch guest from database
+     useEffect(()=>{
+          const fetchGuest =async()=>{
+            const guestRef =  doc(db,"guests",id);
+            const snapShot = await getDoc(guestRef);
+            if(!snapShot.exists()) {
+                router.push("/dashboard/add-guest");
+                return;
+            }
+            setGuest({
+                id,
+                ...snapShot.data(),
+            });
+          };
+          fetchGuest();
+     },[id,router]);
+
+        const handleDelete = async()=>{
+           const confirmDelete = window.confirm("Are you sure you want to delete this guest?");
+           if (!confirmDelete) return; 
+             try {
+                  const deleteRef = doc(db,"guests",id);
+                   await deleteDoc(deleteRef);
+                   alert("Guest deleted successfully");
+                   router.push("/dashboard/guest-list");
+             }
+             catch(errors) {
+                console.error("Error deleting guest:",errors);
+                alert("Failed to delete guest. Please try again.");
+             };
+        };
+    
     return (
         <main className="min-h-screen flex justify-center px-2 md:py-20 md:px-12 lg:py-20 lg:px-20">
             <div className="w-full md:w-[350px] h-[400px] shadow-md rounded-md py-5 px-5">
@@ -8,28 +50,29 @@ export default function GuestPage () {
                 <div className="flex flex-col gap-4 mt-5">
                     <div className="flex justify-between">
                         <p className="text-gray-800 text-xl">Guest Name</p>
-                        <p className="text-gray-600 text-sm mt-2">Augustine Emmanuel</p>
+                        <p className="text-gray-600 text-sm mt-2">{guest?.fullname}</p>
                     </div>
                     <div className="flex justify-between">
                         <p className="text-gray-800 text-xl">Phone Number</p>
-                        <p className="text-gray-600 text-sm mt-2">+2349046758368</p>
+                        <p className="text-gray-600 text-sm mt-2">{guest?.phoneNumber}</p>
                     </div>
                     <div className="flex justify-between">
                         <p className="text-gray-800 text-xl">RoomType</p>
-                        <p className="text-gray-600 text-sm mt-2">Deluxe Room</p>
+                        <p className="text-gray-600 text-sm mt-2">{guest?.roomType} </p>
                     </div>
                      <div className="flex justify-between">
                         <p className="text-gray-800 text-xl">RoomNumber</p>
-                        <p className="text-gray-600 text-sm mt-2">001</p>
+                        <p className="text-gray-600 text-sm mt-2">{guest?.roomNumber}</p>
                     </div>
                      <div className="flex justify-between">
                         <p className="text-gray-800 text-xl">CheckInDate</p>
-                        <p className="text-gray-600 text-sm mt-2">25/12/2025</p>
+                        <p className="text-gray-600 text-sm mt-2">{guest?.checkInDate}</p>
                     </div>   
                 </div>
-                <div className="mt-5">
-                    <button className=" w-[60px]  h-[50px] bg-red-500 text-white flex justify-center items-center rounded-md shadow-md cursor-pointer">
-                        <RiDeleteBin5Line className="text-2xl"/>
+                <div 
+                onClick={handleDelete} className="mt-5">
+                    <button  className=" w-[60px]  h-[50px] bg-red-500 text-white flex justify-center items-center rounded-md shadow-md cursor-pointer">
+                        <RiDeleteBin5Line  className="text-2xl"/>
                     </button>
                 </div>
 
